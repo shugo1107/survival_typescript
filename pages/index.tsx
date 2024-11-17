7024c1fac0ccba645c26b6578e4b767b48652b47
shugo <shugoaoki.1107@gmail.com>
2024-11-17 20:06:05 +0900

    fix: button message

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const IndexPage: NextPage<Props> = ({initialImage}) => {
     }
     return (
     <div className={styles.page}>
-        <button onClick={handleClick} className={styles.button}>New Cat</button>
+        <button onClick={handleClick} className={styles.button}>One more cat!</button>
         <div className={styles.frame}>{loading || <img src={image} />}</div>
     </div>
     );
