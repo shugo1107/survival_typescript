955ce843edd5053b9ac52106787057c6473fd48b
shugo <shugoaoki.1107@gmail.com>
2024-11-17 16:45:46 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,52 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from './index.module.css';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+type Props = {
+    initialImage: string;
+}
+
+const IndexPage: NextPage<Props> = ({initialImage}) => {
+    const [image, setImage] = useState(initialImage);
+    const [loading, setLoading] = useState(false);
+    // useEffect(() => {
+    //     fetchImage().then((image) => {
+    //         setImage(image.url);
+    //         setLoading(false);
+    //     });
+    // }, []);
+    const handleClick = async () => {
+        setLoading(true);
+        const image = await fetchImage();
+        setImage(image.url);
+        setLoading(false);
+    }
+    return (
+    <div className={styles.page}>
+        <button onClick={handleClick} className={styles.button}>New Cat</button>
+        <div className={styles.frame}>{loading || <img src={image} />}</div>
+    </div>
+    );
+};
 
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImage: image.url
+        }
+    };
+};
+
+type Image = {
+    url: string;
+};
+
+const fetchImage = async () : Promise<Image> => {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await response.json();
+    console.log(data);
+    return data[0];
+}
